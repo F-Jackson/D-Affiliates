11410a37f5e6a6366169bb572d1ae7f2a9f45ef9
F. Jackson <102330247+F-Jackson@users.noreply.github.com>
2026-01-03 01:20:15 +0000

    TYUJY

--- src/modules/affiliated.module.ts
+++ src/modules/affiliated.module.ts
@@ -16,6 +16,8 @@ import { AffiliatesController } from 'src/controllers/grpc.controller';
 import { DbModule } from './db.module';
 import { RedisModule } from './redis.module';
 import IdempotencyCheckService from 'src/security/idempotency-check.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -29,15 +31,17 @@ import IdempotencyCheckService from 'src/security/idempotency-check.service';
     ]),
     RedisModule,
     DbModule,
-    {
-      name: 'SERVICES_AFFILIATES_PACKAGE',
-      transport: Transport.GRPC,
-      options: {
-        package: 'services_affiliates',
-        protoPath: join(process.cwd(), 'proto/services_affiliates.proto'),
-        url: process.env.GRPC_URL_AFFILIATES,
+    ClientsModule.register([
+      {
+        name: 'SERVICES_AFFILIATES_PACKAGE',
+        transport: Transport.GRPC,
+        options: {
+          package: 'services_affiliates',
+          protoPath: join(process.cwd(), 'proto/services_affiliates.proto'),
+          url: process.env.GRPC_URL_SERVICES_AFFILIATES,
+        },
       },
-    },
+    ]),
   ],
   providers: [
     AffiliatedService,
